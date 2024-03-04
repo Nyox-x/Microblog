f97398b2c8df111aaafdbd4691c02cf863342d06
Z <Eos571@outlook.com>
2024-03-04 21:49:49 +0800

    c4

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
-from flask import render_template,flash,redirect
+from flask import render_template,flash,redirect,url_for
+from app import app
+from app.forms import LoginForm
 @app.route('/login',methods=['GET','POST'])
 def login():
     form =LoginForm()
